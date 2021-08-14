f936ec4919e5e28dc473207b09cd296ff457a756
hanjianlee <hanjianlee7@gmail.com>
2021-08-14 21:00:38 +0800

    Split Tests
    Split tests into Dedicated Sections

--- client/src/components/DriverCountSlider.test.tsx
+++ client/src/components/DriverCountSlider.test.tsx
@@ -15,15 +15,47 @@ test("Render Driver Count Slider ", () => {
   const text = utils.getByLabelText(
     "driver-count-text"
   ) as HTMLParagraphElement;
-  /** Default Value **/
-  expect(text.textContent).toBe(defaultValue);
+  //   expect(defaultValue).toBeInTheDocument();
   expect(text).toBeInTheDocument();
   expect(input).toBeInTheDocument();
+});
 
+test("Test Count Slider Default Value", () => {
+  const defaultValue = "5";
+  const utils = render(
+    <DriverCountSlider
+      minValue={10}
+      maxValue={0}
+      count={defaultValue}
+      onChange={(event) => event.target?.value}
+    />
+  );
+  const input = utils.getByLabelText("driver-count-slider") as HTMLInputElement;
+  const text = utils.getByLabelText(
+    "driver-count-text"
+  ) as HTMLParagraphElement;
+  /** Default Value **/
+  expect(text.textContent).toBe(defaultValue);
+  expect(input.value).toBe(defaultValue);
+});
+
+test("Test Count Slider Update Value", () => {
+  const defaultValue = "5";
+  const utils = render(
+    <DriverCountSlider
+      minValue={10}
+      maxValue={0}
+      count={defaultValue}
+      onChange={(event) => event.target?.value}
+    />
+  );
+  const input = utils.getByLabelText("driver-count-slider") as HTMLInputElement;
+  const text = utils.getByLabelText(
+    "driver-count-text"
+  ) as HTMLParagraphElement;
   /**Updated  Value **/
   const updatedValue = "10";
   fireEvent.change(input, { target: { value: updatedValue } });
-  //   expect(defaultValue).toBeInTheDocument();
-  expect(input.value).toBe(updatedValue);
   expect(text.textContent).toBe(updatedValue);
+  expect(input.value).toBe(updatedValue);
 });
