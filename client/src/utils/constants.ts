713ade8653df48d1a9d20a66a8ce459a86f5a332
hanjianlee <hanjianlee7@gmail.com>
2021-08-15 00:18:48 +0800

    Fix White Spaces

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,20 +1,23 @@
 /** Just Global Constants **/
-export const MULTIPART_FORM_DATA:string = "multipart/form-data";
-export const APPLICATION_JSON:string="applciation/json";
-export const {REACT_APP_API_URL,REACT_APP_API_KEY,REACT_APP_MAP_GL_STYLE,REACT_APP_MAP_GL_TOKEN} = process.env 
-export const API_PREFIX:string = "api"
+export const MULTIPART_FORM_DATA: string = "multipart/form-data";
+export const APPLICATION_JSON: string = "applciation/json";
+export const {
+  REACT_APP_API_URL,
+  REACT_APP_API_KEY,
+  REACT_APP_MAP_GL_STYLE,
+  REACT_APP_MAP_GL_TOKEN,
+} = process.env;
+export const API_PREFIX: string = "api";
 export type HQLOCATIONTYPE = {
-        country:string,
-        longitude:number,
-        latitude:number,
-}
-export const HQLOCATIONS:HQLOCATIONTYPE[] = [
-    {
-        country: "Singapore",
-        longitude:103.8522982,
-        latitude:1.285194,
-    },
-    {   country: "London",
-        longitude:-0.0964509,
-        latitude:51.5049375,
-    }]
+  country: string;
+  longitude: number;
+  latitude: number;
+};
+export const HQLOCATIONS: HQLOCATIONTYPE[] = [
+  {
+    country: "Singapore",
+    longitude: 103.8522982,
+    latitude: 1.285194,
+  },
+  { country: "London", longitude: -0.0964509, latitude: 51.5049375 },
+];
