ec25922ab4e625202f72df8541cefe90afe7878e
hanjianlee <hanjianlee7@gmail.com>
2021-08-15 17:32:33 +0800

    Move Driver Get Api into Routes and Controllers

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,11 @@
+/** Express **/
 import express from "express";
-import { port, client, splitApi } from "./utils/env";
+/** ENV Constants **/
+import { port, client } from "./utils/env";
+/** Libraries **/
 import cors from "cors";
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+/** Routers **/
+import router from "./routes";
 const app = express();
 app.use(express.json());
 
@@ -11,38 +15,16 @@ const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
 app.use(cors(options));
-
 /** Server Front End React SPA Production Version **/
 app.use(express.static(__dirname + "/build"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/build/index.html");
 });
-app.get("/api/drivers/getDrivers", async (req, res) => {
-  try {
-    const { count, longitude, latitude } = req.query;
-    if (!count)
-      return res
-        .status(400)
-        .json({ message: "Please provide Count - Number of Drivers" });
-    if (!longitude)
-      return res.status(400).json({ message: "Please provide Longitude" });
-    if (!latitude)
-      return res.status(400).json({ message: "Please provide Latitude" });
-    await axios
-      .get(`${splitApi}`, {
-        params: { count, latitude, longitude },
-      } as AxiosRequestConfig)
-      .then((result: AxiosResponse) => {
-        try {
-          res.status(200).json(result.data);
-        } catch (err) {
-          res.status(400).json({ message: err });
-        }
-      });
-  } catch (err) {
-    return res.status(400).json({ message: "Please provide Latitude" });
-  }
-});
+
+/** Use Routers **/
+app.use("/", router);
+
+/** Listening To Port **/
 app.listen(port, () => {
   console.log(`Backend Server running on http://localhost:${port} !`);
 });
