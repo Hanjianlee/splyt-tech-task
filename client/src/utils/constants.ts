b6f2c344b0c6b28124218312abf19873bc8a56eb
hanjianlee <hanjianlee7@gmail.com>
2021-08-15 22:17:42 +0800

    Change HQLOCATIONTYPE Constants to Camel Back notation , fix Whitespaces

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,6 +1,11 @@
 /** Just Global Constants **/
 export const MULTIPART_FORM_DATA: string = "multipart/form-data";
 export const APPLICATION_JSON: string = "applciation/json";
+export const MINIMUM_DRIVERS: number = 1;
+export const MAXIMUM_DRIVERS: number = 10;
+export const MINIMUM_ZOOM: number = 10;
+export const MAXIMUM_ZOOM: number = 20;
+export const DEFAULT_ZOOM: number = 18;
 export const {
   REACT_APP_API_URL,
   REACT_APP_API_KEY,
@@ -12,14 +17,18 @@ export type LocationType = {
   longitude: number;
   latitude: number;
 };
-export type HQLOCATIONTYPE = LocationType & {
+export type HQLocationType = LocationType & {
   country: string;
 };
-export const HQLOCATIONS: HQLOCATIONTYPE[] = [
+export const HQLOCATIONS: HQLocationType[] = [
   {
     country: "Singapore",
     longitude: 103.8522982,
     latitude: 1.285194,
   },
-  { country: "London", longitude: -0.0964509, latitude: 51.5049375 },
+  {
+    country: "London",
+    longitude: -0.0964509,
+    latitude: 51.5049375,
+  },
 ];
